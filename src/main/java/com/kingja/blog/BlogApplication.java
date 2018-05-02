25bbb58b17890b89789381733c23588e7792e35b
KingJA <kingjavip@gmail.com>
2018-05-03 00:23:43 +0800

    发布war

--- src/main/java/com/kingja/blog/BlogApplication.java
+++ src/main/java/com/kingja/blog/BlogApplication.java
@@ -2,7 +2,9 @@ package com.kingja.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,16 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @ServletComponentScan //过滤器
 @EnableCaching //redis缓存
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello Spring Boot";
+        return "Hello Blog";
     }
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
 
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BlogApplication.class);
+    }
 }
